871de328ceba3939066752526fbbd7b3ebeb35fa
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-02-23 21:59:14 +0900

    .vueのコンポーネントのテストを追加（一部パスしない）

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,18 @@
     <h2>カウンター</h2>
     <h3>Count: {{ count }}</h3>
     <button type="button" @click="increment">Increment</button>
+    <app-toggle-button/>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import AppToggleButton from '~/components/AppToggleButton'
 
 export default {
+  components: {
+    AppToggleButton
+  },
   computed: {
     ...mapGetters(['count'])
   },
---
+++ spec/components/AppToggleButton.spec.js
@@ -0,0 +1,24 @@
+import AppToggleButton from '~/components/AppToggleButton.vue'
+import { mount } from '@vue/test-utils'
+
+
+describe('AppToggleButton.vue', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = null
+
+    wrapper = mount(AppToggleButton)
+  })
+
+  test('デフォルト状態で off であるか', () => {
+    expect(wrapper.find('p').text()).toBe('off')
+  })
+
+  // TODO: Pass しない
+  test('ボタンを押すことによって on となるか', () => {
+    wrapper.find('button').trigger('click')
+
+    expect(wrapper.find('p').text()).toBe('on')
+  })
+})
